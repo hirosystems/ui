abb5bb402d161d9eb118e1e3148095bebdf7d816
Kyran Burraston <kyran.burraston@gmail.com>
2019-10-02 14:40:49 +0200

    refactor: TextComponent types

--- src/text/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { BoxProps } from "../box";
-import * as React from "react";
-
-declare const Text: React.FC<BoxProps>;
-
-export default Text;
--- src/text/index.tsx
+++ src/text/index.tsx
@@ -2,7 +2,7 @@ import React, { forwardRef } from "react";
 import Box from "../pseudo-box";
 
 // if nothing is passed for the prop `textStyle`, we will assume styles for various dom elements
-const assumeTextStyle = ({ as }) => {
+const assumeTextStyle = ({ as }: { as: string }) => {
   switch (as) {
     case "h1":
       return "display.large";
@@ -11,10 +11,10 @@ const assumeTextStyle = ({ as }) => {
     case "h3":
       return "body.large.medium";
     default:
-      return undefined;
+      return;
   }
 };
-const Text = forwardRef(({ textStyle, ...rest }, ref) => {
+const Text = forwardRef<any, any>(({ textStyle, ...rest }, ref) => {
   return (
     <Box
       ref={ref}
