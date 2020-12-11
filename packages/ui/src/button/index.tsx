a16dc8b852cc4161947d05f0b3dd9f42467b2366
Thomas Osmonson <aulneau@users.noreply.github.com>
2020-12-11 14:11:36 -0600

    fix: button improvements

--- packages/ui/src/button/index.tsx
+++ packages/ui/src/button/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Box, BoxProps } from '../box';
+import { Flex } from '../flex';
 import { transition } from '@stacks/ui-theme';
 import { color } from '@stacks/ui-utils';
 import { ForwardRefExoticComponentWithAs, forwardRefWithAs } from '@stacks/ui-core';
@@ -14,83 +15,109 @@ export const focusBlue = (alpha = 1) => `rgba(170, 179, 255,${alpha})`;
 export interface ButtonProps extends Omit<BoxProps, 'size'> {
   variant?: 'link' | 'solid';
   mode?: 'primary' | 'secondary' | 'tertiary';
-  /**
-   * If `true`, the button will be disabled.
-   */
   isDisabled?: boolean;
-  /**
-   * The label to show in the button when `isLoading` is true
-   * If no text is passed, it only shows the spinner
-   */
   loadingText?: string;
   isLoading?: boolean;
   type?: 'button' | 'reset' | 'submit';
-  size: 'sm' | 'md' | 'lg';
+  size?: 'sm' | 'md' | 'lg';
 }
 
-const modeStyles = (mode: ButtonProps['mode']) => {
-  switch (mode) {
-    case 'secondary':
-      return ({
-        isPressed,
-        _hover,
-      }: {
-        isPressed?: boolean;
-        isDisabled?: boolean;
-        isLoading?: boolean;
-        _hover?: any;
-      }) => ({
-        bg: blue(0.2, isPressed),
-        border: '1px solid',
-        borderRadius: '6px',
-        color: color('invert'),
-        borderColor: blue(0),
-        _hover: { bg: blue(0.25, true), cursor: 'pointer', ..._hover },
-        _focus: {
-          boxShadow: `0 0 0 3px ${focusBlue(0.5)}`,
-        },
-      });
-    case 'tertiary':
-      return ({
-        isPressed,
-        _hover,
-      }: {
-        isPressed?: boolean;
-        isDisabled?: boolean;
-        isLoading?: boolean;
-        _hover?: any;
-      }) => ({
-        border: '1px solid',
-        borderRadius: '6px',
-        color: color('invert'),
-        bg: color(isPressed ? 'bg' : 'bg-2'),
-        borderColor: color('border'),
-        boxShadow: 'mid',
-        _hover: { bg: color('bg-3'), boxShadow: 'high', cursor: 'pointer', ..._hover },
-        _focus: {
-          boxShadow: `0 0 0 3px ${focusBlue(0.5)}`,
-        },
-      });
-    default:
-      return ({
-        isPressed,
-        _hover,
-      }: {
-        isPressed?: boolean;
-        isDisabled?: boolean;
-        isLoading?: boolean;
-        _hover?: any;
-      }) => ({
-        border: '1px solid',
-        borderRadius: '6px',
-        bg: blue(1, isPressed),
-        color: 'white',
-        borderColor: blue(0),
-        _hover: { bg: blue(1, true), cursor: 'pointer', ..._hover },
-        _focus: {
-          boxShadow: `0 0 0 3px ${focusBlue(0.75)}`,
-        },
-      });
+const modeStyles = (mode: ButtonProps['mode'], variant: ButtonProps['variant'] = 'solid') => {
+  if (variant === 'solid') {
+    switch (mode) {
+      case 'secondary':
+        return ({
+          isPressed,
+          isDisabled,
+          _hover,
+        }: {
+          isPressed?: boolean;
+          isDisabled?: boolean;
+          isLoading?: boolean;
+          _hover?: any;
+        }) => ({
+          bg: blue(0.2, isPressed),
+          border: '1px solid',
+          borderRadius: '6px',
+          color: color('invert'),
+          borderColor: blue(0),
+          _hover: {
+            bg: blue(0.25, true),
+            cursor: isDisabled ? 'not-allowed' : 'pointer',
+            ..._hover,
+          },
+          _focus: {
+            boxShadow: `0 0 0 3px ${focusBlue(0.5)}`,
+          },
+        });
+      case 'tertiary':
+        return ({
+          isPressed,
+          isDisabled,
+          _hover,
+        }: {
+          isPressed?: boolean;
+          isDisabled?: boolean;
+          isLoading?: boolean;
+          _hover?: any;
+        }) => ({
+          border: '1px solid',
+          borderRadius: '6px',
+          color: color('invert'),
+          bg: color(isPressed ? 'bg' : 'bg-2'),
+          borderColor: color('border'),
+          boxShadow: 'mid',
+          _hover: {
+            bg: color('bg-3'),
+            boxShadow: 'high',
+            cursor: isDisabled ? 'not-allowed' : 'pointer',
+            ..._hover,
+          },
+          _focus: {
+            boxShadow: `0 0 0 3px ${focusBlue(0.5)}`,
+          },
+        });
+      default:
+        return ({
+          isPressed,
+          isDisabled,
+          _hover,
+        }: {
+          isPressed?: boolean;
+          isDisabled?: boolean;
+          isLoading?: boolean;
+          _hover?: any;
+        }) => ({
+          border: '1px solid',
+          borderRadius: '6px',
+          bg: blue(1, isPressed),
+          color: 'white',
+          borderColor: blue(0),
+          _hover: { bg: blue(1, true), cursor: isDisabled ? 'not-allowed' : 'pointer', ..._hover },
+          _focus: {
+            boxShadow: `0 0 0 3px ${focusBlue(0.75)}`,
+          },
+        });
+    }
+  } else {
+    return ({
+      _hover,
+      isPressed,
+      isDisabled,
+    }: {
+      isPressed?: boolean;
+      isDisabled?: boolean;
+      isLoading?: boolean;
+      _hover?: any;
+    }) => ({
+      color: blue(1, isPressed),
+      textDecoration: 'none',
+      _hover: {
+        textDecoration: 'underline',
+        cursor: isDisabled ? 'not-allowed' : 'pointer',
+        ..._hover,
+      },
+    });
   }
 };
 
@@ -100,7 +127,7 @@ const sizeProps = (size: 'sm' | 'md' | 'lg') => {
       return {
         px: 'tight',
         py: 'extra-tight',
-        fontSize: '12px,',
+        fontSize: '12px',
       };
     }
     case 'md': {
@@ -112,8 +139,8 @@ const sizeProps = (size: 'sm' | 'md' | 'lg') => {
     }
     case 'lg': {
       return {
-        px: 'loose',
-        py: 'base-loose',
+        px: 'base-loose',
+        py: 'base',
         fontSize: '14px',
       };
     }
@@ -129,7 +156,7 @@ export const Button: ForwardRefExoticComponentWithAs<ButtonProps, 'button'> = fo
     as = 'button',
     onClick,
     mode = 'primary',
-    variant,
+    variant = 'solid',
     isLoading,
     isDisabled,
     size = 'md',
@@ -146,8 +173,8 @@ export const Button: ForwardRefExoticComponentWithAs<ButtonProps, 'button'> = fo
   const { onKeyUp, onKeyDown } = pressProps;
 
   const sizeStyles = variant === 'link' ? {} : sizeProps(size);
-  const modeStyle = modeStyles(mode);
-
+  const _modeStyle = modeStyles(mode, variant);
+  const modeStyle = _modeStyle({ isPressed, _hover, isDisabled });
   return (
     <Box
       as={as}
@@ -163,13 +190,13 @@ export const Button: ForwardRefExoticComponentWithAs<ButtonProps, 'button'> = fo
       onClick={e => {
         onClick?.(e);
       }}
-      display="inline-flex"
+      opacity={isDisabled ? 0.4 : 1}
       alignItems="center"
       justifyContent="center"
-      opacity={isDisabled ? 0.4 : 1}
-      {...rest}
-      {...modeStyle({ isPressed, _hover, isDisabled })}
+      {...modeStyle}
       {...sizeStyles}
+      {...rest}
+      display={rest.display || 'inline-flex'}
     >
       <Grid
         transition={transition}
@@ -180,11 +207,20 @@ export const Button: ForwardRefExoticComponentWithAs<ButtonProps, 'button'> = fo
         left={0}
         size="100%"
         placeItems="center"
-        {...modeStyle({ isPressed: false })}
       >
-        <Spinner size="sm" color={'white'} />
+        <Spinner size="sm" color="currentColor" />
       </Grid>
-      {children}
+      <Flex
+        transition="opacity 0.2s cubic-bezier(0.23, 1, 0.32, 1)"
+        as="span"
+        color="currentColor"
+        opacity={isLoading ? 0 : 1}
+        display={rest.display || 'inline-flex'}
+        alignItems={rest.alignItems || 'center'}
+        justifyContent={rest.justifyContent || 'center'}
+      >
+        {children}
+      </Flex>
     </Box>
   );
 });
